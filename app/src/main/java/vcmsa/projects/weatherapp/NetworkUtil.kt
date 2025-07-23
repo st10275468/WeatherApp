44556fc01d27eadf880066ca6055fbf3cd5a6a28
Thomas <st10275468@vcconnect.edu.za>
2025-07-23 08:26:49 +0200

    Start of WeatherApp

---
+++ app/src/main/java/vcmsa/projects/weatherapp/NetworkUtil.kt
@@ -0,0 +1,7 @@
+package vcmsa.projects.weatherapp
+
+    private val WEATHERBASEURL = ""
+    private val PARAMMETRIC = "metric"
+    private val METRICVALUE = "true"
+    private val PARAMAPIKEY = "apikey"
+    private val lOGGINGTAG = "URLWECREATED"
